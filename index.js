1f19c34c3c4919fb07d27aa3a99546fa55b73ff2
Aaron <AaronBatilo@gmail.com>
2017-04-26 17:13:54 -0600

    Reading in a raw HTML file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const winston = require('winston');
+const fs = require('fs');
 
 const app = express();
 
@@ -13,6 +14,15 @@ app.get('/', (req, res) => {
 
 const server = app.listen(PORT, () => {
   winston.info(`Example app listening on port ${PORT}`);
+});
+
+let rawHTML = '';
+fs.readFile('./sample_html.html', 'utf8', (err, data) => {
+    if (err) {
+      winston.error('There was an error trying to read the sample file');
+    }
+    rawHTML = data;
+    winston.info(rawHTML);
 });
 
 module.exports.server = server;
