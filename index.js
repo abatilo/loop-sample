74dd7ff5bcdd8453b20dfa0d52bee3b2752098db
Aaron <AaronBatilo@gmail.com>
2017-04-26 18:45:30 -0600

    Learning how htmlparser2 does its stream parsing

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-const express = require('express');
+/* const express = require('express'); */
 const winston = require('winston');
 const fs = require('fs');
+const htmlparser = require('htmlparser2');
 
-const app = express();
+/* const app = express(); */
 
 // Let Heroku decide the port number to use
-const PORT = process.env.PORT || 8080;
+/* const PORT = process.env.PORT || 8080; */
 
-app.get('/', (req, res) => {
-  winston.info('Request made to root');
-  res.send('Looplist Challenge');
-});
+/* app.get('/', (req, res) => { */
+/*   winston.info('Request made to root'); */
+/*   res.send('Looplist Challenge'); */
+/* }); */
 
-const server = app.listen(PORT, () => {
-  winston.info(`Example app listening on port ${PORT}`);
-});
+/* const server = app.listen(PORT, () => { */
+/*   winston.info(`Example app listening on port ${PORT}`); */
+/* }); */
+
+/* module.exports.server = server; */
+
+let onTitle = false;
+
+const parser = new htmlparser.Parser({
+  onopentag: (name) => {
+    if (name === 'title') {
+      onTitle = true;
+    }
+  },
+
+  ontext: (text) => {
+    if (onTitle) {
+      winston.info(text);
+    }
+  },
+
+  onclosetag: (name) => {
+    if (name === 'title') {
+      onTitle = false;
+    }
+  },
+}, { decodeEntities: true });
 
 let rawHTML = '';
 fs.readFile('./sample_html.html', 'utf8', (err, data) => {
@@ -22,7 +47,8 @@ fs.readFile('./sample_html.html', 'utf8', (err, data) => {
     winston.error('There was an error trying to read the sample file');
   }
   rawHTML = data;
-  winston.info(rawHTML);
+
+  parser.write(rawHTML);
+  parser.end();
 });
 
-module.exports.server = server;
